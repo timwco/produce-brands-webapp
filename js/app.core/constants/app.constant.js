87456b047c0b35cb8ff22bd1891813e9cc6f456f
Tim Whitacre <tim@timw.co>
2016-01-07 21:44:12 -0500

    Updated production url

--- js/app.core/constants/app.constant.js
+++ js/app.core/constants/app.constant.js
@@ -1,6 +1,6 @@
 // Define URL's (add trailing slash)
 let development = 'http://localhost:3000/';
-let production = '';
+let production = 'http://api.producebrands.com/';
 
 export default {
   URL: (window.location.href.indexOf("localhost")) ? development : production,
